94e6cabf32d52e69ddfb4adf20c1aab0e0ab4ed7
vishal <vishalrathod7038@gmail.com>
2021-11-10 08:43:55 +0530

    [vishal] Address Book Uc2 Add new contact in Address book

---
+++ Address bookUc2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Address_bookUc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddDetails Data = new AddDetails();
+            Data.Details();
+            Console.ReadLine();
+
+        }
+    }
+}
