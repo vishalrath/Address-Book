b9bc5bb18cb496186e8322d672e8d92ceb2d799b
vishal <vishalrathod7038@gmail.com>
2021-11-10 08:47:52 +0530

    [vishal] Address book uc3  edit contact

---
+++ Address Book uc3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Address_Book_uc3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            edit editdetail = new edit();
+            editdetail.editdetails("vishal");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Address Book uc3/edit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_uc3
+{
+    class edit
+    {
+        public string firstName = "vishal";
+        public string lastName = "Rathod";
+        public string address = "Washim";
+        public string city = "Pune";
+        public string state = "Maharstra";
+        public string mobNo = "7038762577";
+
+        List<String> obj = new List<String>(); //define List data structure
+
+        public void editdetails(string firstName)  //matching element edited in list data structure
+        {
+            Console.WriteLine("\nEditing the existing name: ");
+            if (this.firstName == firstName)
+            {
+                Console.WriteLine("\n Enter new name: ");
+                obj.Add(Console.ReadLine());   //edit name element
+                Console.WriteLine("\n Edited Name Sucessfully..");
+            }
+            else
+            {
+                Console.WriteLine("\n Don't edited to existing data..");
+            }
+        }
+    }
+}
