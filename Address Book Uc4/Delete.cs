a0767664ce205c4d339fceaa3bc1892798686bfb
vishal <vishalrathod7038@gmail.com>
2021-11-10 08:51:11 +0530

    [vishal] Address Book Uc4 Delete person using name in address Book

---
+++ Address Book Uc4/Delete.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Uc4
+{
+    class Delete
+    {
+        public string firstName = "vishal";
+        public string lastName = "rathod";
+        public string address = "washim";
+        public string city = "Pune";
+        public string state = "Maharstra";
+        public string mobNo = "7038762577";
+
+        List<String> obj = new List<String>(); //define List data structure
+
+        public void remove(string firstName)  //matching element deleted in list data structure
+        {
+            if (this.firstName == firstName)
+            {
+                obj.Remove(firstName); //remove name element
+                Console.WriteLine("\n Name Successfully deleted...");
+            }
+            else
+            {
+                Console.WriteLine("\n Not deleted...");
+            }
+        }
+    }
+}
---
+++ Address Book Uc4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Address_Book_Uc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delete delete = new Delete();
+            delete.remove("Vishal");
+        }
+    }
+}
