2cf4e6b097d7d8e6ec230a8013936f37f576e5c6
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-21 00:00:42 +0530

    [vishal] Uc9_ ability to view Cityname And StateName in andressbook

---
+++ Uc9_AbilityToVeiw_PersonTocityAndState/CustomExcepetion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc9_AbilityToVeiw_PersonTocityAndState
+{
+    class CustomExcepetion:Exception
+    {
+        private readonly ExceptionType type;
+        public enum ExceptionType
+        {
+            WrongCityName,
+            WrongStateName
+        }
+
+        public CustomExcepetion(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
