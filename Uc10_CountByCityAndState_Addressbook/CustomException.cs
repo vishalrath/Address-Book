8a3fc3b0b68dcbf13e27fa88ffafa4a3221589a5
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-21 00:02:34 +0530

    [vishal] Uc9_ Count by Cityname And StateName in andressbook

---
+++ Uc10_CountByCityAndState_Addressbook/CustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc10_CountByCityAndState_Addressbook
+{
+    class CustomException:Exception
+    {
+        private readonly ExceptionType type;
+        public enum ExceptionType
+        {
+            WrongCityName,
+            WrongStateName
+        }
+
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
+
