4e02a4fe35d06c20382c2b5b35aa1ad658cd3a4b
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-24 23:25:03 +0530

    [vishal] Csv File operation on Address book

---
+++ Uc14_AddressBook_CSVFileOperation/CustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc14_AddressBook_CSVFileOperation
+{
+    class CustomException:Exception
+    {
+        private readonly ExceptionType type;
+        public enum ExceptionType
+        {
+            WrongCityName,
+            WrongStateName
+        }
+
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
